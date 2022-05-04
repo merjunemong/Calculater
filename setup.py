d42e38930421228bf5efc2aee9af41e11233ecb1
merjunemong <100757595+merjunemong@users.noreply.github.com>
2022-05-04 10:49:15 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+ipmort setuptools
+
+setuptools.setup(
+  inlclude_package_data = True,
+  name = 'textoutput1116',
+  version='0.0.1',
+  description='oss-development class text output'
+  author='sejinchun',
+  author_email='kdjun016@naver.com'
+  url = 'https://github.com/merjunemong/textoutput/'
+  install_requires=['pytest'],
+  long_description = 'oss-development ...',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python ::3",
+    "Operating System "" OS Independent",
+  ],
+)
